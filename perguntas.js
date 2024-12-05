64adbc8cde30dde5f40eef977494332f416898f4
nika-joyboy <00001108115251sp@al.educacao.sp.gov.br>
2024-12-05 16:15:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Jogos',
+    'Quem é a protagonista de Jogos?',
+    'voce mesmo'
+)
+
+criaCartao(
+    'Jogos',
+    'O que são os Jogos Vorazes?',
+    'Uma competição mortal organizada pela Capital para controlar os distritos.'
+)
+
+criaCartao(
+    'Jogos',
+    'Quem acompanha Katniss nos Jogos?',
+    'Peeta Mellark, que também é do Distrito 12 e tem um papel crucial na trama.'
+)
+
+criaCartao(
+    'Jogos ',
+    'Qual é o tema central da série?',
+    'Luta contra a opressão '
+)
